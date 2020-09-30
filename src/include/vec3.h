f1c31b6e650737be5e65dd0520af1d0ab6c4210e
Sage Livingstone <sagewlivingstone@gmail.com>
2020-09-30 18:31:17 -0500

    added sphere to scene, fixed dot product issue

--- src/include/vec3.h
+++ src/include/vec3.h
@@ -96,7 +96,7 @@ inline double dot(const vec3& u, const vec3& v)
 {
     return u.e[0] * v.e[0]
         + u.e[1] * v.e[1]
-        + u.e[2] + v.e[2];
+        + u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3& u, const vec3& v)
