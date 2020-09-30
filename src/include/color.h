979f5fc82378787ca4cf7afea9c0c4aa404c12a4
Sage Livingstone <sagewlivingstone@gmail.com>
2020-09-30 15:54:22 -0500

    folder structuring

---
+++ src/include/color.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "include/vec3.h"
+
+#include <iostream>
+
+void write_color(std::ostream& out, color pixel_value)
+{
+    out << static_cast<int>(255.999 * pixel_value.x()) << ' '
+        << static_cast<int>(255.999 * pixel_value.y()) << ' '
+        << static_cast<int>(255.999 * pixel_value.z()) << '\n';
+}
+
+#endif // !COLOR_H
+
