a851ecd438160ecd01a55c374f2a3032040517ef
Sage Livingstone <sagewlivingstone@gmail.com>
2020-10-03 16:03:23 -0500

    abstracted ray hittable class

---
+++ src/include/sphere.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class Sphere : public hittable {
+public:
+    Sphere() {}
+    Sphere(point3 cen, double r) : center(cen), radius(r) {}
+
+    virtual bool hit(
+        const ray& r, double tmin, double tmax, hit_record& rec) const override;
+
+public:
+    point3 center;
+    double radius;
+};
+
+bool Sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+
+    if (discriminant > 0) {
+        auto root = sqrt(discriminant);
+
+        // TODO: Don't always check for normal? Waste of compute if already hit
+
+        // Check the negative root
+        auto tmp = (-half_b - root) / a;
+        if (tmp < tmax && tmp > tmin) {
+            rec.t = tmp;
+            rec.p = r.at(tmp);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+
+        // Check the positive root
+        auto tmp = (-half_b + root) / a;
+        if (tmp < tmax && tmp > tmin) {
+            rec.t = tmp;
+            rec.p = r.at(tmp);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+
+        return false;
+    }
+
+    return false;
+}
+
+#endif
