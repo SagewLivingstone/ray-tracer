17cc2dd1ad71b4f91a40938968db11a7ecc34ebb
Sage Livingstone <sagewlivingstone@gmail.com>
2020-10-03 18:24:57 -0500

    true lambertian reflection

--- src/include/vec3.h
+++ src/include/vec3.h
@@ -125,11 +125,10 @@ inline vec3 interp3(const vec3& a, const vec3& b, double x)
 }
 
 vec3 random_in_unit_sphere() {
-    while (true) {
-        auto p = vec3::random(-1, 1);
-        if (p.length_squared() >= 1) continue;
-        return p;
-    }
+    auto a = random_double(0, 2 * pi);
+    auto z = random_double(-1, 1);
+    auto r = sqrt(1 - z * z);
+    return vec3(r * cos(a), r * sin(a), z);
 }
 
 #endif
