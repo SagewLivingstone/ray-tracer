acf0a85fea1b0caf90d3aa5655a5814c177cbbd0
Sage Livingstone <sagewlivingstone@gmail.com>
2020-10-03 18:08:25 -0500

    gamma 2 correction

--- src/include/color.h
+++ src/include/color.h
@@ -14,9 +14,9 @@ void write_color(std::ostream& out, color pixel_value, int samples_per_pixel)
 
     // TODO: Why the heck are we doing the division here? Shouldn't this be in the mainloop?
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt(r * scale);
+    g = sqrt(g * scale);
+    b = sqrt(b * scale);
 
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
