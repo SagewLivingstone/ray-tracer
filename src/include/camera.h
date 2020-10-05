dd9f750b617463a026e73754609e5552e34c0055
Sage Livingstone <sagewlivingstone@gmail.com>
2020-10-05 13:29:20 -0500

    camera movement, aperture, defocus, demo scene

--- src/include/camera.h
+++ src/include/camera.h
@@ -7,24 +7,39 @@
 class camera {
 public:
     camera(
+        point3 lookfrom,
+        point3 lookat,
+        vec3 vup,
         double vfov,
-        double aspect_ratio
+        double aspect_ratio,
+        double aperture,
+        double focus_dist
     ) {
         auto theta = deg_to_rad(vfov);
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;
 
-        auto focal_length = 1.0;
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
 
-        origin = point3(0, 0, 0);
-        horizontal = vec3(viewport_width, 0.0, 0.0);
-        vertical = vec3(0.0, viewport_height, 0.0);
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+        origin = lookfrom;
+        horizontal = focus_dist * viewport_width * u;
+        vertical = focus_dist * viewport_height * v;
+        lower_left_corner = origin - (horizontal / 2 + vertical / 2 + focus_dist * w);
+
+        lens_radius = aperture / 2;
     }
 
-    ray get_ray(double u, double v) const {
-        return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+    ray get_ray(double s, double t) const {
+        vec3 rd = lens_radius * random_in_unit_disk();
+        vec3 offset = rd.x() * u + rd.y() * v;
+
+        return ray(
+            origin + offset,
+            lower_left_corner + s*horizontal + t*vertical - (origin + offset)
+        );
     }
 
 private:
@@ -32,6 +47,8 @@ private:
     point3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
+    vec3 u, v, w;
+    double lens_radius;
 };
 
 #endif // !CAMERA_H
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,54 @@
 
 #include <iostream>
 
+hittable_list demo_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<Sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) {
+                    //metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+                else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<Sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<Sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<Sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 color ray_color(const ray& r, const hittable& world, int depth)
 {
     if (depth <= 0) {
@@ -33,30 +81,37 @@ int main()
 {
 
     // Image
-    const auto aspect_ratio = 16.0 / 9.0;
+    const auto aspect_ratio = 3.0 / 2.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
-    hittable_list world;
+    auto world = demo_scene();
+    //hittable_list world;
 
-    auto material_ground   = make_shared<lambertian>(color(0.1, 0.8, 0.2));
-    auto material_diff     = make_shared<lambertian>(color(0.9, 0.1, 0.7));
-    auto material_metal    = make_shared<metal>(color(0.8, 0.8, 0.8), 0.2);
-    auto material_bronze   = make_shared<metal>(color(0.8, 0.6, 0.2), 0.5);
-    auto material_glass    = make_shared<dielectric>(1.5);
+    //auto material_ground   = make_shared<lambertian>(color(0.1, 0.8, 0.2));
+    //auto material_diff     = make_shared<lambertian>(color(0.9, 0.1, 0.7));
+    //auto material_metal    = make_shared<metal>(color(0.8, 0.8, 0.8), 0.2);
+    //auto material_bronze   = make_shared<metal>(color(0.8, 0.6, 0.2), 0.5);
+    //auto material_glass    = make_shared<dielectric>(1.5);
 
-    world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100.0, material_ground));
-    world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_diff));
-    world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_bronze));
-    world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_glass));
-    world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), -0.45, material_glass));
-    world.add(make_shared<Sphere>(point3(0.0, 1.0, -1.0), 0.5, material_metal));
+    //world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100.0, material_ground));
+    //world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_diff));
+    //world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_bronze));
+
+    //world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_glass));
+    //world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_glass));
+
+    //world.add(make_shared<Sphere>(point3(0.0, 1.0, -1.5), 0.5, material_metal));
 
     // Camera
-    camera cam(60, aspect_ratio);
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
+    double dist_to_focus = 10;
+    camera cam(lookfrom, lookat, vup, 25, aspect_ratio, 0.1, dist_to_focus);
 
     // Render
 
