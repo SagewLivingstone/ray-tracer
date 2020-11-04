ce39d822d9ab3862487683d29323e552be982151
Sage Livingstone <sagewlivingstone@gmail.com>
2020-11-03 20:01:32 -0600

    Fixed bounding box bug

--- src/include/aabb.h
+++ src/include/aabb.h
@@ -41,9 +41,9 @@ aabb surrounding_box(aabb box0, aabb box1) {
         fmin(box0.min().z(), box1.min().z())
     );
     point3 big(
-        fmax(box0.min().x(), box1.min().x()),
-        fmax(box0.min().y(), box1.min().y()),
-        fmax(box0.min().z(), box1.min().z())
+        fmax(box0.max().x(), box1.max().x()),
+        fmax(box0.max().y(), box1.max().y()),
+        fmax(box0.max().z(), box1.max().z())
     );
     return aabb(small, big);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -85,31 +85,31 @@ int main()
 
     // Image
     const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 400;
+    const int image_width = 720;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;  // Maxiumum reflection depth
 
     // World
-    //bvh_node world(demo_scene(4), 0, 1.0);
-    hittable_list world;
+    bvh_node world(demo_scene(8), 0, 1.0);
+    //hittable_list world;
 
-    auto material_ground   = make_shared<lambertian>(color(0.1, 0.8, 0.2));
-    auto material_diff     = make_shared<lambertian>(color(0.9, 0.1, 0.7));
-    auto material_metal    = make_shared<metal>(color(0.8, 0.8, 0.8), 0.2);
-    auto material_bronze   = make_shared<metal>(color(0.8, 0.6, 0.2), 0.5);
-    auto material_glass    = make_shared<dielectric>(1.5);
+    //auto material_ground   = make_shared<lambertian>(color(0.1, 0.8, 0.2));
+    //auto material_diff     = make_shared<lambertian>(color(0.9, 0.1, 0.7));
+    //auto material_metal    = make_shared<metal>(color(0.8, 0.8, 0.8), 0.2);
+    //auto material_bronze   = make_shared<metal>(color(0.8, 0.6, 0.2), 0.5);
+    //auto material_glass    = make_shared<dielectric>(1.5);
 
-    world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100.0, material_ground));
-    world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_diff));
-    world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_bronze));
+    //world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100.0, material_ground));
+    //world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_diff));
+    //world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_bronze));
 
-    world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_glass));
-    world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_glass));
+    //world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_glass));
+    //world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_glass));
 
-    world.add(make_shared<Sphere>(point3(0.0, 1.0, -1.5), 0.5, material_metal));
+    //world.add(make_shared<Sphere>(point3(0.0, 1.0, -1.5), 0.5, material_metal));
 
-    bvh_node bvh_world(world, 0, 1.0);
+    //bvh_node bvh_world(world, 0, 1.0);
 
     // Camera
     point3 lookfrom(13, 2, 3);
@@ -132,7 +132,7 @@ int main()
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, bvh_world, max_depth);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
