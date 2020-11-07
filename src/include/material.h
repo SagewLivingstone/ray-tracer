cc5d708b33a9087b1176f8cdd0d8dca2a90ba3fa
Sage Livingstone <sagewlivingstone@gmail.com>
2020-11-07 17:39:03 -0600

    better naming of dielectric params

--- src/include/material.h
+++ src/include/material.h
@@ -62,24 +62,24 @@ public:
 
 class dielectric : public material {
 public:
-    dielectric(double ri) : ref_idx(ri) {}
+    dielectric(double index_of_refraction) : ref_idx(index_of_refraction) {}
 
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
     ) const override {
         attenuation = color(1.0, 1.0, 1.0);
-        double etai_over_etat = rec.front_face ? (1.0 / ref_idx) : ref_idx; // ref index get flipped if ray direction is backwards
+        double refraction_ratio = rec.front_face ? (1.0 / ref_idx) : ref_idx; // ref index gets flipped if ray direction is backwards
 
         vec3 unit_direction = unit_vector(r_in.direction());
 
         double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
         double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
-        if (etai_over_etat * sin_theta > 1.0) {
+        if (refraction_ratio * sin_theta > 1.0) {
             vec3 reflected = reflect(unit_direction, rec.normal);
             scattered = ray(rec.p, reflected);
             return true;
         }
-        double reflect_prob = schlick(cos_theta, etai_over_etat);
+        double reflect_prob = schlick(cos_theta, refraction_ratio);
         if (random_double() < reflect_prob)
         {
             vec3 reflected = reflect(unit_direction, rec.normal);
@@ -87,7 +87,7 @@ public:
             return true;
         }
 
-        vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+        vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
         scattered = ray(rec.p, refracted, r_in.time());
         return true;
     }
