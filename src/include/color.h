6094785f8745601e7ef2ac1f090e4f31b8933a6a
Sage Livingstone <sagewlivingstone@gmail.com>
2020-11-07 00:36:32 -0600

    PNG outputgit add .!

--- src/include/color.h
+++ src/include/color.h
@@ -23,5 +23,24 @@ void write_color(std::ostream& out, color pixel_value, int samples_per_pixel)
         << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
+void write_data_color(uint8_t* data, int* index, color pixel_value, int samples_per_pixel)
+{
+    auto r = pixel_value.x();
+    auto g = pixel_value.y();
+    auto b = pixel_value.z();
+
+    // TODO: Why the heck are we doing the division here? Shouldn't this be in the mainloop?
+    auto scale = 1.0 / samples_per_pixel;
+    r = sqrt(r * scale) * (256 * clamp(r, 0.0, 0.999));
+    g = sqrt(g * scale) * (256 * clamp(r, 0.0, 0.999));
+    b = sqrt(b * scale) * (256 * clamp(r, 0.0, 0.999));
+
+    data[(*index * 3)]     = (uint8_t)r;
+    data[(*index * 3) + 1] = (uint8_t)g;
+    data[(*index * 3) + 2] = (uint8_t)b;
+
+    *index += 1;
+}
+
 #endif // !COLOR_H
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include "include/material.h"
 #include "include/bvh.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "lib/stb_image_write.h"
+
 #include <iostream>
 
 hittable_list demo_scene(int gridsize = 11) {
@@ -85,31 +88,34 @@ int main()
 
     // Image
     const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 1280;
+    const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
-    const int max_depth = 50;  // Maxiumum reflection depth
+    const int max_depth = 50;  // Maximum reflection depth
+
+    uint8_t* image_data = new uint8_t[image_width * image_height * 3];
+    int image_index = 0;
 
     // World
-    bvh_node world(demo_scene(9), 0, 1.0);
-    //hittable_list world;
+    //bvh_node world(demo_scene(9), 0, 1.0);
+    hittable_list world;
 
-    //auto material_ground   = make_shared<lambertian>(color(0.1, 0.8, 0.2));
-    //auto material_diff     = make_shared<lambertian>(color(0.9, 0.1, 0.7));
-    //auto material_metal    = make_shared<metal>(color(0.8, 0.8, 0.8), 0.2);
-    //auto material_bronze   = make_shared<metal>(color(0.8, 0.6, 0.2), 0.5);
-    //auto material_glass    = make_shared<dielectric>(1.5);
+    auto material_ground   = make_shared<lambertian>(color(0.1, 0.8, 0.2));
+    auto material_diff     = make_shared<lambertian>(color(0.9, 0.1, 0.7));
+    auto material_metal    = make_shared<metal>(color(0.8, 0.8, 0.8), 0.2);
+    auto material_bronze   = make_shared<metal>(color(0.8, 0.6, 0.2), 0.5);
+    auto material_glass    = make_shared<dielectric>(1.5);
 
-    //world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100.0, material_ground));
-    //world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_diff));
-    //world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_bronze));
+    world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100.0, material_ground));
+    world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_diff));
+    world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_bronze));
 
-    //world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_glass));
-    //world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_glass));
+    world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_glass));
+    world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_glass));
 
-    //world.add(make_shared<Sphere>(point3(0.0, 1.0, -1.5), 0.5, material_metal));
+    world.add(make_shared<Sphere>(point3(0.0, 1.0, -1.5), 0.5, material_metal));
 
-    //bvh_node bvh_world(world, 0, 1.0);
+    bvh_node bvh_world(world, 0, 1.0);
 
     // Camera
     point3 lookfrom(13, 2, 3);
@@ -134,9 +140,16 @@ int main()
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
-            write_color(std::cout, pixel_color, samples_per_pixel);
+            write_data_color(image_data, &image_index, pixel_color, samples_per_pixel);
+            //write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
-    std::cerr << "\nDone.\n";
+    std::cerr << "\nDone scanning.\n";
+
+    std::cout << "Saving output...\n";
+
+    stbi_write_png("image.png", image_width, image_height, 3, image_data, sizeof(uint8_t) * image_width * 3);
+
+    std::cerr << "Done saving output.\n";
 }
